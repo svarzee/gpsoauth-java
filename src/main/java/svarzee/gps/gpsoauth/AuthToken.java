39fc1907ac7f71ebadd91ae0be0c526d0529fbdf
svarzee <svarzee@gmail.com>
2016-07-29 02:29:03 +0200

    Equals, hashCode and toString for AuthToken

--- src/main/java/svarzee/gps/gpsoauth/AuthToken.java
+++ src/main/java/svarzee/gps/gpsoauth/AuthToken.java
@@ -17,4 +17,30 @@ public class AuthToken {
   public long getExpiry() {
     return expiry;
   }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+
+    AuthToken authToken = (AuthToken) obj;
+
+    if (expiry != authToken.expiry) return false;
+    return token != null ? token.equals(authToken.token) : authToken.token == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = token != null ? token.hashCode() : 0;
+    result = 31 * result + (int) (expiry ^ (expiry >>> 32));
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "AuthToken{"
+        + "token='" + token + '\''
+        + ", expiry=" + expiry
+        + '}';
+  }
 }
